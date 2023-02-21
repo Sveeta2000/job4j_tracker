0519e11c467a438b804a974e738747e046351f0f
sveet <sveetlana2000@mail.ru>
2023-02-21 14:47:40 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+/**
+ * @author sveet
+ * @date 21.02.2023
+ */
+public class Cat {
+
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+/**
+ * @author sveet
+ * @date 21.02.2023
+ */
+public class Dog {
+
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
