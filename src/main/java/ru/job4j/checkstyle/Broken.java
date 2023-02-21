e0695c6f8031cfc531a54018d4cee0be7ca97df8
sveet <sveetlana2000@mail.ru>
2023-02-21 13:00:20 +0300

    Правила Checkstyle [#504981 #393562]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -6,11 +6,16 @@ package ru.job4j.checkstyle;
  */
 public class Broken {
 
+    public static final String NEW_VALUE = "";
+
     private int sizeOfEmpty = 10;
 
     public String surname;
 
-    public static final String NEW_VALUE = "";
+    String name;
+
+    Broken() {
+    }
 
     void echo() {
     }
@@ -23,10 +28,5 @@ public class Broken {
 
     void method(int a, int b, int c, int d, int e, int f, int g) {
 
-    }
-
-    String name;
-
-    Broken() {
     }
 }
