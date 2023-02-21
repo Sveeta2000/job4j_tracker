00d0f6441b769c400841e2239ce7c840f605a0ba
sveet <sveetlana2000@mail.ru>
2023-02-21 15:13:54 +0300

    1.3. Вызов метода с аргументами. [#166086]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+/**
+ * @author sveet
+ * @date 21.02.2023
+ */
+public class Jukebox {
+
+    public void music(int position) {
+        switch (position) {
+            case 1 :
+                System.out.println("Пусть бегут неуклюже");
+                break;
+            case 2 :
+                System.out.println("Спокойной ночи");
+                break;
+            default:
+                System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox box = new Jukebox();
+        box.music(1);
+        box.music(2);
+        box.music(0);
+    }
+}
--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -6,21 +6,22 @@ package ru.job4j.oop;
  */
 public class Student {
 
-    public void music() {
+    public void music(String lyrics) {
         System.out.println("Tra tra tra");
     }
 
-    public void song() {
+    public void sing() {
         System.out.println("I believe I can fly");
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.song();
-        petya.song();
-        petya.song();
+        String song = "I believe, I can fly";
+        petya.music(song);
+        petya.music(song);
+        petya.music(song);
+        petya.sing();
+        petya.sing();
+        petya.sing();
     }
 }
