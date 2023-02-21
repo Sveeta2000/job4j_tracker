67de3b14e546cace1fb19af77aba6345a9acfcde
sveet <sveetlana2000@mail.ru>
2023-02-21 14:52:54 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+/**
+ * @author sveet
+ * @date 21.02.2023
+ */
+public class Student {
+
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+}
