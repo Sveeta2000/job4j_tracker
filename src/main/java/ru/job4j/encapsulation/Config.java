f24ba810be32631843cc4effbcfa593740bfcd25
sveet <sveetlana2000@mail.ru>
2023-02-27 19:47:58 +0300

    0. Что такое инкапсуляция. [#3818]

---
+++ src/main/java/ru/job4j/encapsulation/Config.java
@@ -0,0 +1,28 @@
+package ru.job4j.encapsulation;
+
+/**
+ * @author sveet
+ * @date 27.02.2023
+ */
+public class Config {
+
+    private String name;
+    private int position;
+    private String[] properties;
+
+    public Config(String name) {
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println(position);
+    }
+
+    public String getProperty(String key) {
+        return search(key);
+    }
+
+    private String search(String key) {
+        return key;
+    }
+}
