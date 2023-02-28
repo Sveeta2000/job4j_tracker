7736176dbc8cf77b6a75b36650784824c1de8d4d
sveet <sveetlana2000@mail.ru>
2023-02-28 19:23:17 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -66,4 +66,18 @@ public class Tracker {
         }
         return false;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        items[index] = null;
+        int start = index + 1;
+        int len = size - index - 1;
+        System.arraycopy(items, start, items, index, len);
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
 }
