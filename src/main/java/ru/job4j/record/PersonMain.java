eae8123f65df12b5d72ce69edd539939756710fd
sveet <sveetlana2000@mail.ru>
2023-03-06 18:09:19 +0300

    11. Record [#504880]

---
+++ src/main/java/ru/job4j/record/PersonMain.java
@@ -0,0 +1,19 @@
+package ru.job4j.record;
+
+/**
+ * @author sveet
+ * @date 06.03.2023
+ */
+public class PersonMain {
+
+    public static void main(String[] args) {
+        Person person = new Person("Иван", 30);
+        System.out.println("Имя: " + person.getName() + ", Возраст: " + person.getAge());
+        System.out.println(person);
+        PersonRecord record = new PersonRecord("Иван", 30);
+        System.out.println("Имя: " + record.name() + ", Возраст: " + record.age());
+        System.out.println(record);
+        PersonRecord record1 = new PersonRecord("Иван", 30);
+        System.out.println(record.equals(record1));
+    }
+}
---
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -0,0 +1,8 @@
+package ru.job4j.record;
+
+/**
+ * @author sveet
+ * @date 06.03.2023
+ */
+public record PersonRecord(String name, int age) {
+}
