d15d7869907c46ab6ca9c16874a3ced0eb43198f
sveet <sveetlana2000@mail.ru>
2023-04-21 14:58:05 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * @author sveet
+ * @date 21.04.2023
+ */
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String str : text) {
+            if (!check.contains(str)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
