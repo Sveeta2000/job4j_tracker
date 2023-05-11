c7166442c9f7d7dc854895b6bc023222b3eb9c41
sveet <sveetlana2000@mail.ru>
2023-05-11 18:21:08 +0300

    1.1. Анонимные классы [#214142]

---
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -0,0 +1,31 @@
+package ru.job4j.lambda;
+
+/**
+ * @author sveet
+ * @date 11.05.2023
+ */
+public class Attachment {
+    private String name;
+    private int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+                + "name='" + name + '\''
+                + ", size=" + size
+                + '}';
+    }
+}
