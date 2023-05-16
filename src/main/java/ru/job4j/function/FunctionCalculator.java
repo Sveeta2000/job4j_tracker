6725d5563cb652adc283f073b388a692d17e1818
sveet <sveetlana2000@mail.ru>
2023-05-16 15:26:33 +0300

    6. Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/function/FunctionCalculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * @author sveet
+ * @date 16.05.2023
+ */
+public class FunctionCalculator {
+    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> rsl = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            rsl.add(func.apply((double) i));
+        }
+        return rsl;
+    }
+}
