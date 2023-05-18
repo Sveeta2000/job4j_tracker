503ba368fa0108c58bf7d502579b5827f667b666
sveet <sveetlana2000@mail.ru>
2023-05-18 15:17:15 +0300

    9. Генерирование колоды карт. FlatMap [#504873]

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author sveet
+ * @date 18.05.2023
+ */
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                        .map(value -> new Card(suit, value)))
+                .collect(Collectors.toList());
+
+    }
+}
+
